9d493c1b25bb542df50ba876d5f994763df4e374
michaelg <none@none>
2004-12-07 00:18:14 +0000

    --HG-- branch : hscore extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%4014

--- trunk/hscore_types.h
+++ trunk/hscore_types.h
@@ -1,33 +1,6 @@
 #ifndef HSCORE_TYPES_H
 #define HSCORE_TYPES_H
 
-/*
-
-typedef struct Info
-{
-    char name[32];
-    char description[256];
-} Info;
-
-typedef struct Property
-{
-    Info info;
-
-    int value;
-} Property;
-
-typedef struct Item
-{
-    Info info;
-
-    int price;
-    int sellPrice;
-
-    LinkedList propertyList;
-} Item;
-
-*/
-
 typedef enum EventAction
 {
 	//removes event->data amount of the items from the ship's inventory
