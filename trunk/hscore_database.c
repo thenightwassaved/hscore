f96efa8b08d492c19d180dd24bb41300e004aeeb
drbrain <none@none>
2004-12-08 21:49:49 +0000

    Added hscore_mysql Added more empty functions in database.
    --HG--
    branch : hscore
    extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%4021

--- trunk/hscore_database.c
+++ trunk/hscore_database.c
@@ -1,5 +1,6 @@
 #include "asss.h"
 #include "hscore.h"
+#include "hscore_mysql.h"
 #include "hscore_database.h"
 
 //modules
@@ -8,6 +9,10 @@ local Ilogman *lm;
 local Ichat *chat;
 local Iconfig *cfg;
 local Icmdman *cmd;
+local Ihscoremysql *mysql;
+local Iarenaman *aman;
+local Iplayerdata *pd;
+local Imainloop *ml;
 
 //interface prototypes
 local int areShipsLoaded(Player *p);
@@ -27,32 +32,85 @@ local int arenaDataKey;
 local LinkedList itemList;
 local LinkedList itemTypeList;
 
-local void InitPlayerData(Player *p) //called before the player data is touched
+//+-------------------------+
+//|                         |
+//|  Data Access Functions  |
+//|                         |
+//+-------------------------+
+
+local ArenaData * getArenaData(Arena *arena)
 {
 
 }
 
-local void CleanupPlayerData(Player *p) //called to free any allocated data
+local PlayerData *getPlayerData(Player *p)
 {
 
 }
 
-local void LoadPlayerGlobals(Player *p) //fetch globals from MySQL
+//+-------------------------+
+//|                         |
+//|  MySQL Query Callbacks  |
+//|                         |
+//+-------------------------+
+
+local void loadItemsQueryCallback(int status, db_res *result, void *passedData)
 {
 
 }
 
-local void StorePlayerGlobals(Player *p) //store player globals
+local void loadPropertiesQueryCallback(int status, db_res *result, void *passedData)
 {
 
 }
 
-local void InitArenaData(Arena *arena) //called before the arena data is touched
+local void loadEventsQueryCallback(int status, db_res *result, void *passedData)
 {
 
 }
 
-local void CleanupArenaData(Arena *arena) //called when the arena is about to die
+local void loadItemTypesQueryCallback(int status, db_res *result, void *passedData)
+{
+
+}
+
+local void loadPlayerGlobalsQueryCallback(int status, db_res *result, void *passedData)
+{
+
+}
+
+local void loadPlayerShipsQueryCallback(int status, db_res *result, void *passedData)
+{
+
+}
+
+local void loadArenaStoresQueryCallback(int status, db_res *result, void *passedData)
+{
+
+}
+
+local void loadArenaCategoriesQueryCallback(int status, db_res *result, void *passedData)
+{
+
+}
+
+//+--------------------+
+//|                    |
+//|  Unload Functions  |
+//|                    |
+//+--------------------+
+
+local void UnloadPlayerGlobals(Player *p) //called to free any allocated data
+{
+
+}
+
+local void UnloadPlayerShips(Player *p) //called to free any allocated data
+{
+
+}
+
+local void UnloadArenaData(Arena *arena) //called when the arena is about to die
 {
 
 }
@@ -77,6 +135,22 @@ local void UnloadItemTypeList()
 
 }
 
+//+------------------+
+//|                  |
+//|  Load Functions  |
+//|                  |
+//+------------------+
+
+local void LoadPlayerGlobals(Player *p, Arena *arena) //fetch globals from MySQL
+{
+
+}
+
+local void LoadPlayerShips(Player *p, Arena *arena) //fetch globals from MySQL
+{
+
+}
+
 local void LoadCategoryList(Arena *arena)
 {
 
@@ -87,7 +161,17 @@ local void LoadStoreList(Arena *arena)
 
 }
 
-local void LoadItemList()
+local void LoadEvents()
+{
+
+}
+
+local void LoadProperties()
+{
+
+}
+
+local void LoadItemList() //will call LoadProperties() and LoadEvents() when finished
 {
 
 }
@@ -96,6 +180,28 @@ local void LoadItemTypeList() //will call LoadItemList() when finished loading
 {
 
 }
+
+//+-------------------+
+//|                   |
+//|  Store Functions  |
+//|                   |
+//+-------------------+
+
+local void StorePlayerGlobals(Player *p) //store player globals
+{
+
+}
+
+local void StorePlayerShips(Player *p) //store player ships
+{
+
+}
+
+//+---------------------+
+//|                     |
+//|  Command Functions  |
+//|                     |
+//+---------------------+
 
 local helptext_t reloadItemsHelp =
 "Targets: none\n"
@@ -117,6 +223,82 @@ local void storeAllCommand(const char *command, const char *params, Player *p, c
 {
 
 }
+
+//+----------------------+
+//|                      |
+//|  Callback functions  |
+//|                      |
+//+----------------------+
+
+local void allocatePlayerCallback(Player *p, int allocating)
+{
+	if (allocating) //player is being allocated
+	{
+
+	}
+	else //p is being deallocated
+	{
+
+	}
+}
+
+local void playerActionCallback(Player *p, int action, Arena *arena)
+{
+	if (action == PA_CONNECT)
+	{
+		//the player is connecting to the server. not arena-specific.
+	}
+	else if (action == PA_DISCONNECT)
+	{
+		//the player is disconnecting from the server. not arena-specific.
+	}
+	else if (action == PA_PREENTERARENA)
+	{
+		//this is called at the earliest point after a player indicates an
+		//intention to enter an arena.
+		//you can use this for some questionable stuff, like redirecting
+		//the player to a different arena. but in general it's better to
+		//use PA_ENTERARENA for general stuff that should happen on
+	 	//entering arenas.
+	}
+	else if (action == PA_ENTERARENA)
+	{
+		//the player is entering an arena.
+	}
+
+	else if (action == PA_LEAVEARENA)
+	{
+		//the player is leaving an arena.
+	}
+	else if (action == PA_ENTERGAME)
+	{
+		//this is called at some point after the player has sent his first
+		//position packet (indicating that he's joined the game, as
+		//opposed to still downloading a map).
+	}
+}
+
+local void arenaActionCallback(Arena *arena, int action)
+{
+	if (action == AA_CREATE)
+	{
+		//arena is being created
+	}
+	else if (action == AA_DESTROY)
+	{
+		//arena is being destroyed
+	}
+	else if (action == AA_CONFCHANGED)
+	{
+		//do we need to do anything?
+	}
+}
+
+//+-----------------------+
+//|                       |
+//|  Interface Functions  |
+//|                       |
+//+-----------------------+
 
 local int areShipsLoaded(Player *p)
 {
@@ -175,8 +357,12 @@ EXPORT int MM_hscore_database(int action, Imodman *_mm, Arena *arena)
 		chat = mm->GetInterface(I_CHAT, ALLARENAS);
 		cfg = mm->GetInterface(I_CONFIG, ALLARENAS);
 		cmd = mm->GetInterface(I_CMDMAN, ALLARENAS);
+		mysql = mm->GetInterface(I_HSCORE_MYSQL, ALLARENAS);
+		aman = mm->GetInterface(I_ARENAMAN, ALLARENAS);
+		pd = mm->GetInterface(I_PLAYERDATA, ALLARENAS);
+		ml = mm->GetInterface(I_MAINLOOP, ALLARENAS);
 
-		if (!lm || !chat || !cfg || !cmd)
+		if (!lm || !chat || !cfg || !cmd || !mysql || !aman || !pd || !ml)
 		{
 			goto fail;
 		}
@@ -197,9 +383,11 @@ EXPORT int MM_hscore_database(int action, Imodman *_mm, Arena *arena)
 		LLInit(&itemList);
 		LLInit(&itemTypeList);
 
-		//load itemTypes (which leads to loading items)
+		//load itemTypes (which leads to loading items, properties and events)
 
-		//register arena callbacks
+		mm->RegCallback(CB_NEWPLAYER, allocatePlayerCallback, ALLARENAS);
+		mm->RegCallback(CB_PLAYERACTION, playerActionCallback, ALLARENAS);
+		mm->RegCallback(CB_ARENAACTION, arenaActionCallback, ALLARENAS);
 
 		mm->RegInterface(&interface, ALLARENAS);
 
@@ -213,6 +401,11 @@ EXPORT int MM_hscore_database(int action, Imodman *_mm, Arena *arena)
 		mm->ReleaseInterface(chat);
 		mm->ReleaseInterface(cfg);
 		mm->ReleaseInterface(cmd);
+		mm->ReleaseInterface(mysql);
+		mm->ReleaseInterface(aman);
+		mm->ReleaseInterface(pd);
+		mm->ReleaseInterface(ml);
+
 		return MM_FAIL;
 
 	}
@@ -226,15 +419,17 @@ EXPORT int MM_hscore_database(int action, Imodman *_mm, Arena *arena)
 		cmd->RemoveCommand("reloaditems", reloadItemsCommand, ALLARENAS);
 		cmd->RemoveCommand("storeall", storeAllCommand, ALLARENAS);
 
-		//unregister arena callbacks
+		mm->UnregCallback(CB_NEWPLAYER, allocatePlayerCallback, ALLARENAS);
+		mm->UnregCallback(CB_PLAYERACTION, playerActionCallback, ALLARENAS);
+		mm->UnregCallback(CB_ARENAACTION, arenaActionCallback, ALLARENAS);
 
 		//save and unload all player data
 
-		//cleanup all arena data (stores and categories)
+		//unload all arena data (stores and categories)
 
-		//cleanup itemList
+		//unload itemList
 
-		//cleanup itemTypeList
+		//unload itemTypeList
 
 		pd->FreePlayerData(playerDataKey);
 		aman->FreeArenaData(arenaDataKey);
@@ -243,6 +438,10 @@ EXPORT int MM_hscore_database(int action, Imodman *_mm, Arena *arena)
 		mm->ReleaseInterface(chat);
 		mm->ReleaseInterface(cfg);
 		mm->ReleaseInterface(cmd);
+		mm->ReleaseInterface(mysql);
+		mm->ReleaseInterface(aman);
+		mm->ReleaseInterface(pd);
+		mm->ReleaseInterface(ml);
 
 		return MM_OK;
 	}
