7df02e68a1fa7f56147b828fa56fa1af6e068f05
drbrain <none@none>
2005-01-22 03:32:19 +0000

    Added makefiles
    --HG--
    branch : hscore
    extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%40154

---
+++ trunk/hscore.mk
@@ -0,0 +1,16 @@
+hscore.$(SO): modules/hscore/trunk/hscore_buysell.o \
+	modules/hscore/trunk/hscore_commands.o \
+	modules/hscore/trunk/hscore_database.o \
+	modules/hscore/trunk/hscore_items.o \
+	modules/hscore/trunk/hscore_money.o \
+	modules/hscore/trunk/hscore_mysql.o \
+	modules/hscore/trunk/hscore_rewards.o \
+	modules/hscore/trunk/hscore_spawner.o \
+	modules/hscore/trunk/hscore_storeman.o \
+	modules/hscore/trunk/hscore_freqman.o \
+	modules/hscore/trunk/hscore_moneystub.o \
+	modules/hscore/trunk/hscore_itemsstub.o
+hscore.$(SO): LIBS=$(MYSQL_LDFLAGS)
+
+modules/hscore/trunk/hscore_mysql.o: CFLAGS += $(MYSQL_CFLAGS)
+
