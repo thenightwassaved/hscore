a0a946a6f060619442338db4931fa116eb0700ba
drbrain <none@none>
2005-08-21 18:56:26 +0000

    Modified prizer.
    --HG--
    branch : hscore
    extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%40243

--- hscore_prizer.c
+++ hscore_prizer.c
@@ -7,6 +7,24 @@ local Ichat *chat;
 local Igame *game;
 local Ihscoreitems *items;
 local Inet *net;
+local Iarenaman *aman;
+
+local HashTable *inCenterWarper;
+
+local int spawnkey;
+
+typedef struct SpawnData
+{
+	int checkx1;
+	int checky1;
+	int checkx2;
+	int checky2;
+
+	int spawnx1;
+	int spawny1;
+	int spawnx2;
+	int spawny2;
+} SpawnData;
 
 local void prize(Player *p)
 {
@@ -45,22 +63,43 @@ local void prize(Player *p)
 local void Pppk(Player *p, byte *p2, int len)
 {
     struct C2SPosition *pos = (struct C2SPosition *)p2;
-    Target t;
-    t.type = T_PLAYER;
-    t.u.p = p;
 
-    if (p->p_ship == SHIP_SPEC)
-        return;
+	Arena *arena = p->arena;
+	SpawnData *sd = P_ARENA_DATA(arena, spawnkey);
+	Target t;
+	t.type = T_PLAYER;
+	t.u.p = p;
 
-    if (500 < pos->x>>4 && pos->x>>4 < 550 && 500 < pos->y>>4 && pos->y>>4 < 550)
-    {
-		if (p->position.bounty == 0)
-                prize(p);
+	/* handle common errors */
+	if (!arena) return;
 
-		int x = (rand() % 250) + 300;
-		int y = (rand() % 250) + 300;
-        game->WarpTo(&t, x, y);
-    }
+	/* speccers don't get their position sent to anyone */
+	if (p->p_ship == SHIP_SPEC)
+		return;
+
+	if (sd->checkx1 != 0) //module attached to the arena? (maybe a bad test, eh?)
+	{
+		if (sd->checkx1 < pos->x>>4 && pos->x>>4 < sd->checkx2 && sd->checky1 < pos->y>>4 && pos->y>>4 < sd->checky2)
+		{
+			int *last = HashGetOne(inCenterWarper, p->name);
+			if (!last || *last < current_ticks())
+			{
+				afree(last);
+				int * time = amalloc(sizeof(*time));
+				*time = current_ticks() + 200;
+				HashReplace(inCenterWarper, p->name, time);
+
+				int x = (rand() % (sd->spawnx2 - sd->spawnx1)) + sd->spawnx1;
+				int y = (rand() % (sd->spawny2 - sd->spawny1)) + sd->spawny1;
+
+				game->WarpTo(&t, x, y);
+				if (p->position.bounty == 0)
+				{
+					prize(p);
+				}
+			}
+		}
+	}
 }
 
 EXPORT int MM_hscore_prizer(int action, Imodman *_mm, Arena *arena)
@@ -73,17 +112,26 @@ EXPORT int MM_hscore_prizer(int action, Imodman *_mm, Arena *arena)
 		game = mm->GetInterface(I_GAME, ALLARENAS);
 		items = mm->GetInterface(I_HSCORE_ITEMS, ALLARENAS);
 		net = mm->GetInterface(I_NET, ALLARENAS);
+		aman = mm->GetInterface(I_ARENAMAN, ALLARENAS);
 
-		if(!chat || !game || !items || !net)
+		if(!chat || !game || !items || !net || !aman)
 			return MM_FAIL;
 
+		spawnkey = aman->AllocateArenaData(sizeof(SpawnData));
+		if (spawnkey == -1)
+            return MM_FAIL;
+
 		net->AddPacket(C2S_POSITION, Pppk);
+
+		inCenterWarper = HashAlloc();
 
 		return MM_OK;
 	}
 	else if(action == MM_UNLOAD)
 	{
 		net->RemovePacket(C2S_POSITION, Pppk);
+
+		aman->FreeArenaData(spawnkey);
 
 		mm->ReleaseInterface(chat);
 		mm->ReleaseInterface(game);
@@ -92,5 +140,25 @@ EXPORT int MM_hscore_prizer(int action, Imodman *_mm, Arena *arena)
 
 		return MM_OK;
 	}
+	else if (action == MM_ATTACH)
+    {
+		SpawnData *sd = P_ARENA_DATA(arena, spawnkey);
+
+		sd->checkx1 = cfg->GetInt(arena->cfg, "hyperspace", "checkx1", 502);
+		sd->checky1 = cfg->GetInt(arena->cfg, "hyperspace", "checky1", 502);
+		sd->checkx2 = cfg->GetInt(arena->cfg, "hyperspace", "checkx2", 520);
+		sd->checky2 = cfg->GetInt(arena->cfg, "hyperspace", "checky2", 520);
+
+		sd->spawnx1 = cfg->GetInt(arena->cfg, "hyperspace", "spawnx1", 322);
+		sd->spawny1 = cfg->GetInt(arena->cfg, "hyperspace", "spawny1", 322);
+		sd->spawnx2 = cfg->GetInt(arena->cfg, "hyperspace", "spawnx2", 700);
+		sd->spawny2 = cfg->GetInt(arena->cfg, "hyperspace", "spawny2", 700);
+
+        return MM_OK;
+    }
+    else if (action == MM_DETACH)
+    {
+        return MM_OK;
+    }
 	return MM_FAIL;
 }
