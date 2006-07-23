2a5f9f8fd55c5b7ed56f56689d8b4d324d152db6
drbrain@ECEGEEK.COM <drbrain@ECEGEEK.COM>
2006-07-23 20:58:37 +0000

    Fixed a few misc compile warnings.
    --HG--
    branch : hscore
    extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%40360

--- hscore.mk
+++ hscore.mk
@@ -1,8 +1,7 @@
 hscore_mods = hscore_buysell hscore_commands hscore_database \
 	hscore_items hscore_money hscore_mysql hscore_rewards\
 	hscore_spawner hscore_storeman hscore_freqman \
-	hscore_moneystub hscore_itemsstub hscore_teamnames \
-	hscore_prizer
+	hscore_moneystub hscore_itemsstub hscore_teamnames
 
 hscore_libs = $(MYSQL_LDFLAGS) -lm
 
