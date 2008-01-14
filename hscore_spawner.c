3f48d8dcef068546651d0231240412d270a94b8a
drbrain <none@none>
2008-01-14 22:13:29 +0000

    Had gravity and gravity top speed backwards.
    --HG--
    branch : hscore
    extra : convert_revision : svn%3A83151644-f7ff-4d97-bf54-6cb3b30a954f/hscore%40534

--- hscore_spawner.c
+++ hscore_spawner.c
@@ -551,11 +551,11 @@ local void addOverrides(Player *p)
 			int soccerprox = items->getPropertySumNoLock(p, i, "soccerprox", initSoccerProx);
 			clientset->PlayerOverride(p, shipOverrideKeys[i].SoccerBallProximity, soccerprox);
 
-			int initGravSpeed = cfg->GetInt(conf, shipname, "Gravity", 0);
+			int initGravSpeed = cfg->GetInt(conf, shipname, "GravityTopSpeed", 0);
 			int gravspeed = items->getPropertySumNoLock(p, i, "gravspeed", initGravSpeed);
 			clientset->PlayerOverride(p, shipOverrideKeys[i].GravityTopSpeed, gravspeed);
 
-			int initGravity = cfg->GetInt(conf, shipname, "GravityTopSpeed", 0);
+			int initGravity = cfg->GetInt(conf, shipname, "Gravity", 0);
 			int grav = items->getPropertySumNoLock(p, i, "grav", initGravity);
 			clientset->PlayerOverride(p, shipOverrideKeys[i].Gravity, grav);
 
